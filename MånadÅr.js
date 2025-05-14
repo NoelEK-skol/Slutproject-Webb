b4d893abbefcbb19dab3cf2977956253e29858d5
NoelEK-skol <noek07001@utb.vaxjo.se>
2025-05-14 08:49:06 +0200

    fixat js script på månad/ år knapp

--- MånadÅr.js
+++ MånadÅr.js
@@ -1,8 +1,18 @@
 function Månad(){
     var Månad = document.getElementById("Månad");
-    
+    document.getElementById("Månad").style.display = "block";
+    document.getElementById("År").style.display = "none";
+    document.getElementsByClassName("btnM")[0].style.backgroundColor = "Gray";
+    document.getElementsByClassName("btnÅ")[0].style.backgroundColor = "White";
 }
 
 function År(){
     var År = document.getElementById("År");
+    document.getElementById("Månad").style.display = "none";
+    document.getElementById("År").style.display = "block";
+    document.getElementsByClassName("btnÅ")[0].style.backgroundColor = "Gray";
+    document.getElementsByClassName("btnM")[0].style.backgroundColor = "White";
 }
+
+document.getElementById("År").style.display = "none";
+document.getElementsByClassName("btnM")[0].style.backgroundColor = "Gray";
