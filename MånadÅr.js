79321167195156b510b22871d4277a63bb75c2ce
NoelEK-skol <noek07001@utb.vaxjo.se>
2025-05-21 09:40:03 +0200

    lagt till js på flera ställen, gömma/ visa content

--- MånadÅr.js
+++ MånadÅr.js
@@ -16,3 +16,27 @@ function År(){
 
 document.getElementById("År").style.display = "none";
 document.getElementsByClassName("btnM")[0].style.backgroundColor = "Gray";
+
+
+
+
+
+
+function Content1(){
+    var Content1 = document.getElementById("Content1");
+    document.getElementById("Content1").style.display = "block";
+    document.getElementById("Content2").style.display = "none";
+    document.getElementsByClassName("btn1")[0].style.backgroundColor = "Gray";
+    document.getElementsByClassName("btn2")[0].style.backgroundColor = "White";
+}
+
+function Content2(){
+    var Content2 = document.getElementById("Content2");
+    document.getElementById("Content1").style.display = "none";
+    document.getElementById("Content2").style.display = "block";
+    document.getElementsByClassName("btn2")[0].style.backgroundColor = "Gray";
+    document.getElementsByClassName("btn1")[0].style.backgroundColor = "White";
+}
+
+document.getElementById("Content2").style.display = "none";
+document.getElementsByClassName("btn1")[0].style.backgroundColor = "Gray";
